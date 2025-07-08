5587d394af61bf8fa96070a365751385438a6bfa
zieerssystems <rajesh@zieers.com>
2025-07-08 15:54:29 +0530

    Add files via upload

---
+++ config_path.php
@@ -0,0 +1,5 @@
+<?php
+// config_path.php (place in /public_html or /include)
+define('ROOT_PATH', dirname(__DIR__, 1));  // This gives /home/.../publish.zieerseducations.com
+define('CONFIG_PATH', ROOT_PATH . '/private/pub_config.ini');
+?>
